00d791b75633cc13ec6d325a1a66d4bbb9ff0ffc
Viktor Nagy <viktor.nagy@toolpart.hu>
2012-10-04 15:37:22 +0200

    added tests and validation for posts. Fixes #1.

--- src/menuk/resources.py
+++ src/menuk/resources.py
@@ -1,8 +1,10 @@
 from tastypie.authorization import Authorization
 from tastypie.authentication import ApiKeyAuthentication, Authentication
+from tastypie.validation import CleanedDataFormValidation as FormValidation
 from tastypie.resources import ALL, ALL_WITH_RELATIONS, convert_post_to_put, ModelResource
 
 import models
+import forms
 
 class MenuResource(ModelResource):
 
@@ -14,6 +16,7 @@ class MenuResource(ModelResource):
         detail_allowed_methods = ['get']
         list_allowed_methods = ['post', 'get']
         fields = ['menu', 'where', 'from_url', 'day']
+        validation = FormValidation(form_class=forms.DailyMenuForm)
         
         object_class = models.DailyMenu
         authentication = Authentication()
--- src/menuk/tests.py
+++ src/menuk/tests.py
@@ -6,7 +6,37 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from tastypie.test import ResourceTestCase
+import models
 
+class MenuTest(ResourceTestCase):
+
+    def test_post_new_menu(self):
+        post_data = {
+            'where': 'Castro',
+            'menu': 'asdf asdf',
+            'from_url': 'http://index.hu'
+        }
+        self.assertHttpCreated(self.api_client.post('/api/v1/menu/', format='json', data=post_data))
+
+    def test_post_new_menu_required(self):
+        post_data = {
+            'menu': 'asdf asdf',
+            'from_url': 'http://index.hu'
+        }
+        self.assertHttpBadRequest(self.api_client.post('/api/v1/menu/', format='json', data=post_data))
+
+        post_data = {
+            'where': 'Castro',
+            'from_url': 'http://index.hu'
+        }
+        self.assertHttpBadRequest(self.api_client.post('/api/v1/menu/', format='json', data=post_data))
+
+        post_data = {
+            'where': 'Castro',
+            'menu': 'asdf asdf',
+        }
+        self.assertHttpBadRequest(self.api_client.post('/api/v1/menu/', format='json', data=post_data))
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
