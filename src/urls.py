056cc5efbac362a1f78d561e4059e991cc5df997
Viktor Nagy <viktor.nagy@gmail.com>
2012-10-17 13:57:27 +0200

    we have a basic home page now and some links in the vanilla implementation

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 #admin.site = AdminSite()
 admin.autodiscover()
@@ -9,6 +10,9 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 urlpatterns = patterns('',
     url('^_ah/warmup$', 'djangoappengine.views.warmup'),
     # (r'^comments/', include('django.contrib.comments.urls')),
-    url(r'^', include('menuk.urls')),
+    url(r'^menus/', include('menuk.urls')),
+    url(r'^$', TemplateView.as_view(
+        template_name='home.html'
+        ), name='home'),
     url('^admin/', include(admin.site.urls)),
 )
